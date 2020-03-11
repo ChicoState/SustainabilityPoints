bd2dbf1689a660572df00e38f1464e381e0d28d5
Michael Larson <mlarson1@mail.csuchico.edu>
2020-03-11 12:10:56 -0700

    Login Functionality

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -2,55 +2,64 @@ import React, {useState} from 'react';
 import { Alert, LinearGradient, Image, TouchableOpacity, TextInput, Button, StyleSheet, Text, View } from 'react-native';
 import logo from '../assets/SPplaceholder-02.png';
 import { CustomButton } from '../components/CustomButton.js'
-import * as Font from 'expo-font'
 import ProfileScreen from './ProfileScreen';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+import Firebase from './../apis/Firebase'
 
-function LoginScreen ({navigation}) {
+Firebase.auth().onAuthStateChanged((user) => {
+	if (user != null) {
+	  console.log("we are authenticated now!");
+	}
+  });
 
-	return (
+class LoginScreen extends React.Component {
+	constructor(props) {
+		super(props);
+	  	this.state = {
+			email: "",
+			password: ""
+	  	};
+	}
+	Login = (email, password) => {
+		try {
+			Firebase
+				.auth()
+				.signInWithEmailAndPassword(email, password)
+				.then(res => { 
+				   console.log(res.user.email);
+			 	});
+  		} catch (error) {
+			console.log(error.toString(error));
+		}
+	}
+	
+	render() {
+		return (
+			<View style={{padding: 50, backgroundColor: '#FFFFFF'}}>
+				<View>
+					<View style={{ justifyContent: 'center', alignItems: 'center',}}>
+						<Image source={logo} style={{marginBottom: "2%"}} />
+					</View>
 
-		<View style={{padding: 50, backgroundColor: '#FFFFFF'}}>
-		<View>
-		<View style={{
-			justifyContent: 'center',
-				alignItems: 'center',
-		}}>
-		<Image source={logo} style={{marginBottom: "2%"}} />
-		</View>
-
-		<Text style={styles.titleText}>Sustainability Points</Text>
-
-		<TextInput  placeholder="Username"
-		placeholderTextColor = "#4D786E"
-		style={{backgroundColor: 'rgba(247,247,247,0.6)', borderRadius: 5, borderColor: '#00B78D',borderWidth: 1,padding:10,
-				marginBottom: "8%", height:50}}
-		/>
-
-		<TextInput placeholder="Password"
-		placeholderTextColor = "#4D786E"
-		style={{backgroundColor: 'rgba(247,247,247,0.6)',borderRadius: 5, height: 50, borderColor: '#00B78D',borderWidth: 1,padding:10,
-				marginBottom: "8%"}}/>
-
-		<CustomButton
-		title="Login"
-		onPress={() => navigation.navigate('Profile')  }
-		style={{backgroundColor: "#00B78D", }}
-		textStyle={{ color:"#FFF" }}
-		/>
-
-		<View style={{padding:30}}>
-		<Button title="Sign Up" color="#00B78D"
-		onPress={() => navigation.navigate('SignUpScreen')  }/>
-		</View>
-
-		<Button color="#B7002A" title="Forgot Password?"/>
-		</View>
-		</View>
-
-	);
+					<Text style={styles.titleText}>Sustainability Points</Text>
+					<TextInput  placeholder="Username" placeholderTextColor = "#4D786E" style={styles.textbox}/>
+					<TextInput  placeholder="Email"	placeholderTextColor = "#4D786E" style={styles.textbox} 
+						onChangeText={email => this.setState({ email })}/>
+					<TextInput placeholder="Password" placeholderTextColor = "#4D786E" style={styles.textbox}
+						onChangeText={password => this.setState({ password })}/>
+					<CustomButton title="Login" onPress={ () => this.Login(this.state.email, this.state.password) }
+						style={{backgroundColor: "#00B78D", }} 	textStyle={{ color:"#FFF" }}/>
+					<View style={{padding:30}}>
+						<Button title="Sign Up" color="#00B78D"	onPress={() => navigation.navigate('SignUpScreen')  }/>
+					</View>
+					<Button color="#B7002A" title="Forgot Password?"/>
+				</View>
+			</View>
+		);
+	const { navigation } = this.props;
+	}
 }
 
 const styles = StyleSheet.create({
@@ -91,6 +100,18 @@ const styles = StyleSheet.create({
 		color: '#2BB700',
 		marginBottom: "8%"
 	},
+	textbox: {
+		backgroundColor: 'rgba(247,247,247,0.6)', 
+		borderRadius: 5, 
+		borderColor: '#00B78D',
+		borderWidth: 1,
+		padding:10,
+		marginBottom: "8%", 
+		height:50,
+	},
 });
 
-export default LoginScreen;
+export default function(props) {
+	const navigation = useNavigation();
+	return <LoginScreen {...props} navigation = {navigation} />
+}
