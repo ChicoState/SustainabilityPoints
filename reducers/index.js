d3ebb2b98d0330a511f9ed1fd832efef2eaae82d
Anthony Vargas <Anthony@Anthonys-MacBook-Pro-6.local>
2020-04-23 03:04:36 -0700

    Navigation fully working, and other changes/implementations

--- reducers/index.js
+++ reducers/index.js
@@ -1,28 +1,50 @@
+/** @format */
 
-import { combineReducers } from 'redux'
-import { LOGIN, SIGNUP, UPDATE_EMAIL, UPDATE_PASSWORD, UPDATE_DISPLAYNAME, UPDATE_ORGNAME } from '../actions/user'
+import { combineReducers } from "redux";
+import {
+  LOGIN,
+  SIGNUP,
+  UPDATE_EMAIL,
+  UPDATE_PASSWORD,
+  UPDATE_DISPLAYNAME,
+  UPDATE_ORGNAME,
+  LOGINTOKEN,
+  SIGNOUT,
+} from "../actions/user";
 
 const user = (state = {}, action) => {
-    switch (action.type) {
-        case LOGIN:
-            return action.payload
-        case SIGNUP:
-            return action.payload
-        case UPDATE_EMAIL:
-            return { ...state, email: action.payload }
-        case UPDATE_PASSWORD:
-            return { ...state, password: action.payload }
-        case UPDATE_DISPLAYNAME:
-            return { ...state, displayname: action.payload }
-        case UPDATE_ORGNAME:
-            return { ...state, orgname: action.payload }
-        default:
-            return state
-    }
-}
+  switch (action.type) {
+    case LOGIN:
+      return action.payload;
+    case SIGNUP:
+      return action.payload;
+    case UPDATE_EMAIL:
+      return { ...state, email: action.payload };
+    case UPDATE_PASSWORD:
+      return { ...state, password: action.payload };
+    case UPDATE_DISPLAYNAME:
+      return { ...state, displayname: action.payload };
+    case UPDATE_ORGNAME:
+      return { ...state, orgname: action.payload };
+    default:
+      return state;
+  }
+};
+
+const auth = (state = false, action) => {
+  switch (action.type) {
+    case LOGINTOKEN:
+      return !state;
+    case SIGNOUT:
+      return !state;
+    default:
+      return state;
+  }
+};
 
 const rootReducer = combineReducers({
-    user
-})
+  user: user,
+  auth: auth,
+});
 
-export default rootReducer
+export default rootReducer;
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,91 +1,92 @@
 /** @format */
 
 import React from "react";
+import { Image, TextInput, Button, StyleSheet, Text, View } from "react-native";
 import {
-  Image,
-  TextInput,
-  Button,
-  StyleSheet,
-  Text,
-  View
+  ScrollView,
+  KeyboardAvoidingView,
+  Keyboard,
+  Platform,
 } from "react-native";
-import { ScrollView, KeyboardAvoidingView, Keyboard, Platform } from "react-native";
 import logo from "../assets/SPplaceholder-02.png";
 import { CustomButton } from "../components/CustomButton.js";
 import Firebase from "./../apis/Firebase";
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
-
-
-import { updateEmail, updatePassword, login, getUser } from '../actions/user'
-
-import { Colors, Spacing, Typography } from '../styles'
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import {
+  updateEmail,
+  updatePassword,
+  login,
+  getUser,
+  LoginToken
+} from "../actions/user";
+import { Colors, Spacing, Typography } from "../styles";
 
 class LoginScreen extends React.Component {
   componentDidMount = () => {
-    Firebase.auth().onAuthStateChanged(user => {
-        if (user) {
-            this.props.getUser(user.uid)
-            if (this.props.user != null) {
-                this.props.navigation.navigate('Profile')
-            }
+    Firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.props.getUser(user.uid);
+        if (this.props.user != null) {
+          this.props.LoginToken();
         }
-    })
-  }
+      }
+    });
+  };
 
   render() {
     return (
       <ScrollView>
         <KeyboardAvoidingView
-      behavior={Platform.Os == "ios" ? "padding" : "height"}
-      style={styles.container}
-    >
-        <View style={styles.container}>
-          <View>
-            <View style={{ justifyContent: "center", alignItems: "center" }}>
-              <Image source={logo} style={{ marginBottom: "2%" }} />
-            </View>
+          behavior={Platform.Os == "ios" ? "padding" : "height"}
+          style={styles.container}
+        >
+          <View style={styles.container}>
+            <View>
+              <View style={{ justifyContent: "center", alignItems: "center" }}>
+                <Image source={logo} style={{ marginBottom: "2%" }} />
+              </View>
 
-            <Text style={styles.titleText}>Sustainability Points</Text>
+              <Text style={styles.titleText}>Sustainability Points</Text>
 
-            <TextInput
-              placeholder="Email"
-              placeholderTextColor="#4D786E"
-              style={styles.textbox}
-              autoCapitalize="none"
-              onChangeText={email => this.props.updateEmail( email )}
-            />
+              <TextInput
+                placeholder="Email"
+                placeholderTextColor="#4D786E"
+                style={styles.textbox}
+                autoCapitalize="none"
+                onChangeText={(email) => this.props.updateEmail(email)}
+              />
 
-            <TextInput
-              placeholder="Password"
-              placeholderTextColor="#4D786E"
-              style={styles.textbox}
-              autoCapitalize="none"
-              onChangeText={password => this.props.updatePassword( password )}
-            />
+              <TextInput
+                placeholder="Password"
+                placeholderTextColor="#4D786E"
+                style={styles.textbox}
+                autoCapitalize="none"
+                onChangeText={(password) => this.props.updatePassword(password)}
+              />
 
-            <CustomButton
-              title="Login"
-              onPress={() => this.props.login()}
-              style={{ backgroundColor: "#00B78D" }}
-              textStyle={{ color: "#FFF" }}
-            />
+              <CustomButton
+                title="Login"
+                onPress={() => this.props.login()}
+                style={{ backgroundColor: "#00B78D" }}
+                textStyle={{ color: "#FFF" }}
+              />
 
-            <View style={{ padding: 15 }}>
+              <View style={{ padding: 15 }}>
+                <Button
+                  title="Sign Up"
+                  color="#00B78D"
+                  onPress={() => this.SignUpFunc()}
+                />
+              </View>
+
               <Button
-                title="Sign Up"
-                color="#00B78D"
-                onPress={() => this.SignUpFunc()}
+                title="Forgot Password?"
+                color="#B7002A"
+                onPress={() => this.RecoveryPage()}
               />
             </View>
-
-            <Button
-              title="Forgot Password?"
-              color="#B7002A"
-              onPress={() => this.RecoveryPage()}
-            />
           </View>
-        </View>
         </KeyboardAvoidingView>
       </ScrollView>
     );
@@ -108,39 +109,34 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.background,
     alignItems: "center",
     justifyContent: "center",
-    ...Spacing.screen
+    ...Spacing.screen,
   },
   titleText: {
     alignItems: "center",
     justifyContent: "center",
     color: Colors.titleText,
-    ...Typography.titleText
+    ...Typography.titleText,
   },
   textbox: {
     borderRadius: 5,
     borderWidth: 1,
     ...Colors.textbox,
-    ...Spacing.textbox
-  }
+    ...Spacing.textbox,
+  },
 });
 
-const mapDispatchToProps = dispatch => {
-  return bindActionCreators({ updateEmail, updatePassword, login, getUser }, dispatch)
-}
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators(
+    { updateEmail, updatePassword, login, getUser, LoginToken },
+    dispatch
+  );
+};
 
-const mapStateToProps = state => {
-	return {
-		user: state.user
-	}
-}
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+    auth: state.auth,
+  };
+};
 
-export default connect(
-	mapStateToProps,
-	mapDispatchToProps
-)(LoginScreen)
-
-// export default function(props) {
-//   const navigation = useNavigation();
-//   const { signIn } = React.useContext(AuthContext);
-//   return <LoginScreen {...props} navigation={navigation} />;
-// }
+export default connect(mapStateToProps, mapDispatchToProps)(LoginScreen);
