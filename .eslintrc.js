5b7c69becec5c109f48c196773432b445bfcc5b6
Michael Larson <mlarson!@mail.csuchico.edu>
2020-05-06 09:57:19 -0700

    Add support for redux in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,57 +1,51 @@
-// module.exports = {
-//     "env": {
-//         "browser": true,
-//         "react-native/react-native": true,
-//     },
-//     "extends": [
-//         "eslint:recommended",
-//     ],
-//     "globals": {
-//         "Atomics": "readonly",
-//         "SharedArrayBuffer": "readonly"
-//     },
-//     "parserOptions": {
-//         "ecmaFeatures": {
-//             "jsx": true
-//         },
-//         "ecmaVersion": 2018,
-//         "sourceType": "module"
-//     },
-//     "plugins": [
-//         "react",
-//         "react-native"
-//     ],
-//     "ignorePatterns": ["node_modules/", ".expo/", ".vscode"],
-//     "rules": {
-//     }
-// };
 module.exports = {
-  "parser": "babel-eslint",
-  "parserOptions": {
-  "ecmaFeatures": {
-    "jsx": true
-  }
-},
-  "env": {
-    "browser": true,
-    "react-native/react-native": true
-  },
-  "plugins": ["react", "react-native"],
-  "extends": ["eslint:recommended", "plugin:react/recommended"],
-  "ignorePatterns": ["node_modules/", ".expo/", ".vscode/", "*.json", "*.html", "assets/"],
-  "rules": {
-    "react-native/no-unused-styles": 2,
-    "react-native/split-platform-components": 2,
-    "react-native/no-inline-styles": 2,
-    "react-native/no-color-literals": 2,
-    "react-native/no-raw-text": 2,
-    "react-native/sort-styles": [
-      "error",
-      "asc",
-       {
-         "ignoreClassNames": false,
-         "ignoreStyleProperties": false
-       }
-     ]
-  }
+    "parser": "babel-eslint",
+
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        }
+    },
+
+    "env": {
+        "browser": true,
+        "react-native/react-native": true
+    },
+
+    "plugins": [
+        "react", 
+        "react-native",
+        "react-redux"
+    ],
+
+    "extends": [
+        "eslint:recommended", 
+        "plugin:react/recommended",
+    ],
+    
+    "ignorePatterns": [
+        "node_modules/", 
+        ".expo/", 
+        ".vscode/", 
+        "*.json", 
+        "*.html", 
+        "assets/"
+    ],
+    
+    "rules": {
+        "react-native/no-unused-styles": 2,
+        "react-native/split-platform-components": 2,
+        "react-native/no-inline-styles": 2,
+        "react-native/no-color-literals": 2,
+        "react-native/no-raw-text": 2,
+        "react-native/sort-styles": [
+            "error",
+            "asc",
+            {
+                "ignoreClassNames": false,
+                "ignoreStyleProperties": false
+            }
+        ],
+        "react-redux/no-unused-prop-types": 0
+    }
 }
